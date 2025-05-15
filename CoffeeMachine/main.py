20de30456c766bee97b71dd0f4acf200dea34a71
Nisarg Patel <p16238041@my365.dmu.ac.uk>
2025-05-14 21:31:30 -0500

    added refill ingredient's logic

--- CoffeeMachine/main.py
+++ CoffeeMachine/main.py
@@ -24,23 +24,27 @@ MENU = {
     },
 }
 
-profit = 0
+sales = 0
 resources = {
     "water": 300,
     "milk": 200,
     "coffee": 100,
 }
 
+# made a separate copy of machine resources to track ingredients
+current_resources = resources.copy()
+
 # TODO 1. Print report of all coffee machine resources
 def print_report():
-    for key, values in resources.items():
+    for key, values in current_resources.items():
         print(f"{key} : {values}")
 
 # TODO 2. Check the resources sufficient to make drink order.
 def is_resource_sufficient(ordered_drink):
     """Return True when order can be made, False if ingredients are insufficient."""
+
     for item in ordered_drink["ingredients"]:
-        if ordered_drink["ingredients"][item] >= resources.get(item, 0):
+        if ordered_drink["ingredients"][item] > current_resources.get(item, 0):
             print(f"Sorry, there is not enough {item}.")
             return False
     return True
@@ -59,38 +63,51 @@ def process_coins():
 def is_transaction_successful(money_received, drink_cost):
     """Return True when the payment is accepted, or False if money is insufficient"""
     if money_received == drink_cost:
-        global profit
-        profit += drink_cost
+        global sales
+        sales += drink_cost
         return True
     elif money_received > drink_cost:
         change = round(money_received - drink_cost, 2)
         print(f"Here is ${change} in change.")
-        profit += drink_cost
+        sales += drink_cost
         return True
     else:
         print("Sorry that's not enough money. Money refunded")
-        return  False
+        return False
 
 # TODO 5. Deduct resources, restart the code
 def make_coffee(drink_name, ordered_item):
     """Deduct the required ingredients from the resources"""
     for item in ordered_item["ingredients"]:
-        resources[item] -= ordered_item["ingredients"][item]
+        current_resources[item] -= ordered_item["ingredients"][item]
     print(f"Here is your {drink_name}. Enjoy! ☕")
 
+def refill_ingredients(leftover_resources):
+    for item in leftover_resources:
+        if leftover_resources[item] != resources[item]:
+            current_resources[item] += (resources[item] - leftover_resources[item])
+        else:
+            pass
+    print_report()
+    print("Machine has been refilled. Ready to Brew! ")
 
 coffee_machine_is_on = True
 while coffee_machine_is_on:
-    choice = input("What would you like? (espresso/latte/cappuccino): ").lower()
-    if choice == "off":
-        coffee_machine_is_on = False
-        print(f"Machine is OFF")
-    elif choice == "report":
-        print_report()
-        print(f"Money: ${profit}")
-    else:
-        drink = MENU[choice]
-        if is_resource_sufficient(drink) is True:
-            payment = process_coins()
-            if is_transaction_successful(payment, drink["cost"]):
-                make_coffee(choice, drink)
+    try:
+        choice = input("What would you like? (espresso/latte/cappuccino): ").lower()
+        if choice == "off":
+            coffee_machine_is_on = False
+            print(f"Machine is OFF")
+        elif choice == "report":
+            print_report()
+            print(f"Money: ${sales}")
+        elif choice == "refill":
+            refill_ingredients(current_resources)
+        else:
+            drink = MENU[choice]
+            if is_resource_sufficient(drink) is True:
+                payment = process_coins()
+                if is_transaction_successful(payment, drink["cost"]):
+                    make_coffee(choice, drink)
+    except:
+        print("ERROR")
